1cfb916bdea8020e41a9f634214c0a068bb41372
Jorge R Ovalle Z <jroz9105@gmail.com>
2015-01-11 12:53:41 -0500

    First commit

---
+++ Yo IntervengoTests/Yo_IntervengoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Yo_IntervengoTests.swift
+//  Yo IntervengoTests
+//
+//  Created by Jorge Raul Ovalle Zuleta on 1/11/15.
+//  Copyright (c) 2015 Olinguito. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Yo_IntervengoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
