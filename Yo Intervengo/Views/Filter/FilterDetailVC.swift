ea64d42c7567c53fb36d50d060d885310c538926
Jorge Raul Ovalle Zuleta <jroz9105@gmail.com>
2015-05-20 23:00:04 -0500

    Styles filters

--- Yo Intervengo/Views/Filter/FilterDetailVC.swift
+++ Yo Intervengo/Views/Filter/FilterDetailVC.swift
@@ -9,32 +9,53 @@
 import UIKit
 
 class FilterDetailVC: UITableViewController,UIGestureRecognizerDelegate {
-
+    var options = [JSON]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.clearColor()
         self.navigationController?.interactivePopGestureRecognizer.delegate = self
-        // Do any additional setup after loading the view.
+        
+        let backButton = UIBarButtonItem(title: "  Atrás", style: UIBarButtonItemStyle.Plain, target: self, action: "goBack")
+        backButton.tintColor = UIColor.orangeYI()
+        navigationItem.leftBarButtonItem = backButton
+        navigationItem.leftBarButtonItem?.setTitleTextAttributes([NSFontAttributeName: UIFont(name: "Roboto-Regular", size: 16)!], forState: UIControlState.Normal)
     }
     
-    /*override func viewDidAppear(animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(false, animated: true)
-    }*/
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func goBack(){
+        self.navigationController?.view.layer.addAnimation(CATransition.viewTransition(), forKey: nil)
+        self.navigationController?.popViewControllerAnimated(false)
     }
-    */
-
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.cellForRowAtIndexPath(indexPath)?.textLabel?.textColor = UIColor.whiteColor()
+        tableView.cellForRowAtIndexPath(indexPath)?.backgroundColor = UIColor.lightGrayColor()
+    }
+    
+    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.cellForRowAtIndexPath(indexPath)?.textLabel?.textColor = UIColor.blackColor()
+        tableView.cellForRowAtIndexPath(indexPath)?.backgroundColor = UIColor.clearColor()
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "")
+        var jsonObj = options[indexPath.row]
+        cell.textLabel?.text = jsonObj["name"].string
+        cell.textLabel?.font = UIFont(name: "Roboto-Light", size: 20)
+        cell.backgroundColor = UIColor.clearColor()
+        cell.selectionStyle = UITableViewCellSelectionStyle.None
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    override func tableView(tableView: UITableView, indentationLevelForRowAtIndexPath indexPath: NSIndexPath) -> Int {
+        return 0
+    }
 }
--- Yo Intervengo/Views/Filter/FilterMasterVC.swift
+++ Yo Intervengo/Views/Filter/FilterMasterVC.swift
@@ -10,54 +10,60 @@ import UIKit
 
 class FilterMasterVC: UITableViewController,UITableViewDelegate,APIManagerDelegate {
     var APIManagerClass:APIManager!
-    var filters:NSMutableArray!
+    var filters = [JSON]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         APIManagerClass = APIManager()
         APIManagerClass.delegate = self
         APIManagerClass.getFilter()
-        // Do any additional setup after loading the view.
     }
-    
-    /*override func viewDidAppear(animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(true, animated: true)
-    }*/
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         var detail = FilterDetailVC()
+        var jsonObj = filters[indexPath.row]
+        detail.options = jsonObj["data"].array!
+        print(detail.options)
         self.navigationController?.view.layer.addAnimation(CATransition.viewTransition(), forKey: nil)
         self.navigationController?.pushViewController(detail, animated: false)
     }
     
     func returnObt(responseObject:AnyObject, url:String){
-        //print(responseObject)
         var json = JSON(data: responseObject as! NSData)
-        
-//        print(json["filters"])
-        
         for filter in json["filters"]{
-            println(filter.1["name"])
-            println(filter.1["data"][0]["name"])
-            println("----")
+            filters.append(filter.1)
         }
     }
     
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "")
+        var jsonObj = filters[indexPath.row]
+        cell.textLabel?.text = jsonObj["name"].string
+        cell.textLabel?.font = UIFont(name: "Roboto-Light", size: 20)
+        cell.tintColor = UIColor.blackColor()
+        cell.backgroundColor = UIColor.clearColor()
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        return cell
     }
-    */
+    
+   
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filters.count
+    }
+    
+    override func tableView(tableView: UITableView, indentationLevelForRowAtIndexPath indexPath: NSIndexPath) -> Int {
+        return 0
+    }
+    
+    /*override func tableView(tableView: UITableView, accessoryTypeForRowWithIndexPath indexPath: NSIndexPath!) -> UITableViewCellAccessoryType {
+        UITableViewCellAccessoryType.DisclosureIndicator
+    }*/
+    
     
 }
